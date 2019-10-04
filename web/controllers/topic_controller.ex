722fddc54a99744ceb705a2f00ea6f856af889e2
Khondokar Hashibul Islam <peeyalk@hotmail.com>
2019-10-04 13:01:49 +0900

    flash message and redirection in topic controller

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -8,7 +8,6 @@ defmodule Discuss.TopicController do
     render(conn, "index.html", topics: topics)
   end
 
-  
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render(conn, "new.html", changeset: changeset)
@@ -16,9 +15,12 @@ defmodule Discuss.TopicController do
 
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
+
     case Repo.insert(changeset) do
-      {:ok, topic} ->
-        IO.inspect(topic)
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
 
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
